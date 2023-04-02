4cdcc545b80cac62b4a5e321173f9a785f62b5a6
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-04-02 13:20:35 +0700

    lesson_2_code comment

---
+++ lesson_2_code_comment/script.js
@@ -0,0 +1,12 @@
+/* 
+    This project for JavaScript
+    Zero to Hero
+*/
+// Loggin Say Hello
+console.log('Hello Zeeker'); // Hello Zeeker
+// console.log();
+/* 
+    End
+    This project for JavaScript
+    Zero to Hero
+*/
