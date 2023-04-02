0f9be7f6b08708e9da03af554b7f6433a101f705
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-04-02 13:59:24 +0700

    lesson 4 data type

---
+++ lesson_4_datatype/script.js
@@ -0,0 +1,19 @@
+/*
+    Data Type
+    keyword typeof
+*/
+// String
+let a = 'Hi';
+console.log(typeof a); // string
+// Number
+let b = 12;
+console.log(typeof b); // number
+// Boolean
+let c = true;
+console.log(typeof c); // boolean
+// Undefined
+let d;
+console.log(typeof d); // undefined
+// Null 
+let e = null;
+console.log(e); // null
