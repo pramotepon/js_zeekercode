6887cffc1d81c7ffdd822df502e7e965be323039
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-04-02 13:42:58 +0700

    Variable

---
+++ lesson_3_variables/script.js
@@ -0,0 +1,23 @@
+/* 
+    var, let, const
+*/
+// Let variable a assign 3
+let a = 3;
+// Logging Variable a
+console.log(`Variable a = ${a}`); // Variable a = 3
+// Constant variable b assign 10
+const b = 10;
+// Logging Variable b
+console.log('Variable b =', b); // Variable b = 10
+// New assign 5 into variable a
+a = 5;
+// Logging new assign Variable a
+console.log(`Variable a new assign = ${a}`); // Variable a new assign = 5
+// New assign 15 into variable b
+b = 15;
+// Logging new assign Variable b
+console.log(`Variable b new assign = ${b}`); // Error
+// Let Variable c is Empty
+let c;
+// Logging variable c
+console.log(`Variable c = ${c}`); //
