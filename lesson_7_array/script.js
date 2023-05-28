35115abf0d94f3e95db7b590aefb487ba235aa41
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-05-28 22:07:47 +0700

    Array and loop for array

---
+++ lesson_7_array/script.js
@@ -0,0 +1,45 @@
+/* 
+    const myArr = ["Ant", "Bee", "cat", 12, true];
+    Array index start of 0 to n
+
+*/
+let myLetArr = ["let_1","let_2","let_3"];
+
+const myArr = ["Ant", "Bee", "cat", 12, true];
+
+// const myNestArr = [ [1, 2] , 3 , [4, 5] ];
+
+// console.log(myNestArr[0][0]); // 1
+// console.log(myNestArr[0][1]); // 2
+
+// variableName[index]
+console.log(myArr);
+
+myArr[2] = "Cat";
+
+console.log(myArr);
+
+// console.log(myLetArr);
+
+// myLetArr = ["new_let_1","new_let_2","new_let_3"];
+
+// console.log(myLetArr);
+
+// myLetArr[0] = "New_let_1";
+
+// console.log(myLetArr);
+
+// myArr = ["new_Ant", "new_Bee", "new_cat", 12, true];
+
+// console.log(myArr.length); // 5
+// console.log(myLetArr);
+
+// myLetArr.push("let_4");
+
+// console.log(myLetArr);
+
+// const myPop = myLetArr.pop();
+
+// console.log(myPop);
+
+// console.log(myLetArr);
---
+++ lesson_8_loop/index.js
@@ -0,0 +1,73 @@
+// Array
+// The initialization; The condition; The afterthought
+const color = ["Red", "Blue", "Green", "Yellow", "Black", "White"];
+// console.log(color.length-1);
+// color[0] color[1] color[2] color[3];
+/* 
+  For loop
+*/ 
+// init = 0; 0 < 3; i + 1
+// 0; true; { log i } after 0 + 1;
+// 1; true; { log i } after 1 + 1;
+// 2; true; { log i } after 2 + 1;
+// 3; false; 
+// end loop
+// console.log(color[0]);
+// console.log(color[1]);
+// console.log(color[2]);
+// console.log(color[3]);
+// for(let i = 0; i < 3; i++){
+//   console.log(i);
+// }
+// for(let i = 0; i < 5; i++){
+//   console.log(color[i]);
+// }
+// for(let i = 0; i < color.length; i++){
+//   console.log(color[i]);
+// }
+
+/* 
+  Foreach
+*/
+// colorName = "Red", colorName = "Blue" ... colorName = "White"
+// color.forEach((colorName, index, colorArray) => {
+//   console.log('Index:' + index);
+//   console.log('Color: ' + colorName);
+//   console.log('Color Array: ' + colorArray);
+// });
+
+/* 
+  While
+*/
+// let i = 0;
+// while (i > color.length) {
+//   console.log(color[i]);
+//   i++;
+// }
+/* 
+  Do while
+*/
+// do {
+//   console.log(color[i]);
+//   i++;
+// } while (i > color.length);
+/* 
+  Continue
+*/
+// 0, 1, 3,..... , 9
+// for(let i = 0; i < 10; i++){
+//   if(i === 2){
+//     console.log('Two');
+//     continue;
+//   }
+//   console.log(i);
+// }
+/* 
+  Break
+*/
+for(let i = 0; i < 10; i++){
+  if(i === 2){
+    break;
+  }
+  console.log(i);
+}
