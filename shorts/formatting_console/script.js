a10b8881f8660518b847e7f6b75cef13ce54e1a1
pramotepon <40195236+pramotepon@users.noreply.github.com>
2023-07-20 13:26:00 +0700

    Short: Formatting console

---
+++ shorts/formatting_console/script.js
@@ -0,0 +1,7 @@
+// Formatting Console
+console.log('%cHello World', 'color: red; font-size: xx-large; font-weight:bold;');
+
+console.log('%cZeeker %cCode %cHello World'
+, 'font-size: xx-large; font-weight:bold;'
+, ''
+, 'color: red; font-size: xx-large; font-weight:bold;');
